d45c9e0ac664205bfca1bd75bcc76dcaf3704ff3
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-06-21 04:59:24 -0700

    edit: f_pint.c

--- f_pint.c
+++ f_pint.c
@@ -10,6 +10,9 @@ void f_pint(stack_t **head, unsigned int line_number)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
