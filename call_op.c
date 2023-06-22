0609556042260a2c9f1167e727bb801c4ee38e9c
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-06-22 18:18:58 +0100

    Update call_op.c

--- call_op.c
+++ call_op.c
@@ -19,7 +19,7 @@ void call_op(stack_t **stack)
 		opcode = strtok(global.line, " \n\t\r\a");
 		global.arg = strtok(NULL, " \n\t");
 		if (opcode == NULL || *opcode == '#')
-			return (0);
+			continue;
 		exec_op(opcode, stack, line_number);
 	}
 }
