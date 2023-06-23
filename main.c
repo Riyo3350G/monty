647fe800aa165ddfd938b73806850a9fa52b55a9
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-06-23 12:17:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	if (global.file == NULL)
 	{
 		fflush(stdout);
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
